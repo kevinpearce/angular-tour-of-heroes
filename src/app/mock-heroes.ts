af7e9d0be8f9628715a6270f8c76cd20bb2d84f5
Kevin Pearce <akevinp@Engineering.ukho.gov.uk>
2022-04-01 16:28:06 +0100

    feat(*): convert to list display - create mock hero list - convert static display to list - add click event handler

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Stricker' },
+    { id: 12, name: 'Porter' },
+    { id: 13, name: 'Byrnes' },
+    { id: 14, name: 'Hoder' },
+    { id: 15, name: 'Mihelich' },
+    { id: 16, name: 'Girard' },
+    { id: 17, name: 'Bennett' },
+    { id: 18, name: 'Hamilton' },
+    { id: 19, name: 'Reynolds' },
+    { id: 20, name: 'Moeller' },
+];
